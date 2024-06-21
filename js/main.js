deb47404c8653504c8ef2316e791c4061e2e49ea
Kirill <pesterevkv2003@gmail.com>
2024-06-21 22:15:49 +0300

    upd: finish

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function generateCSS() {
+  let inset = document.getElementById("inside").checked;
+  inset = inset ? "inset " : "";
+
+  let translateX = document.getElementById("translateX").value;
+
+  let translateY = document.getElementById("translateY").value;
+
+  let blur = document.getElementById("blur").value;
+
+  let stretch = document.getElementById("stretching").value;
+
+  let color = document.getElementById("color").value;
+  let colorR = color.slice(1, 3);
+  let colorG = color.slice(3, 5);
+  let colorB = color.slice(5, 7);
+
+  let opacity = document.getElementById("opacity").value;
+
+  let resultCode = `${translateX}px ${translateY}px ${blur}px ${stretch}px
+  rgba(${parseInt(colorR, 16)}, ${parseInt(colorG, 16)}, ${parseInt(
+    colorB,
+    16
+  )}, ${opacity}) ${inset}`;
+  resultCSS.innerHTML = `
+  box-shadow: ${resultCode};
+  `;
+
+  let example = document.querySelector(".result");
+  example.style.boxShadow = resultCode;
+}
+generateCSS();
+
+addEventListener("input", generateCSS);
+addEventListener("change", generateCSS);
